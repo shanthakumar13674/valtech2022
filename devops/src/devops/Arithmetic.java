a73b2879809e255f8ef98f0f394e9158cb6092a1
shanthakumar <shanthakumar@gmail.com>
2022-09-30 15:06:46 +0530

    Working HelloWorld.java

---
+++ devops/src/devops/Arithmetic.java
@@ -0,0 +1,5 @@
+package devops;
+
+public interface Arithmetic {
+
+}
--- devops/src/devops/HelloWorld.java
+++ devops/src/devops/HelloWorld.java
@@ -1,5 +1,9 @@
 package devops;
 
 public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 
 }
